10a70df0fe5b3e23f4e4ba11d00ad0940fbc17ef
Fábio Gomes <fabioxgn@gmail.com>
2014-05-14 20:19:21 -0300

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 go-bot-heroku
 =============
 
-My go-bot deployed to heroku
+[![Build Status](https://travis-ci.org/fabioxgn/go-bot-heroku.png?branch=master)](https://travis-ci.org/fabioxgn/go-bot-heroku)
+
+This is an example of how to deploy a go-bot app to heroku.
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/fabioxgn/go-bot"
+	_ "github.com/fabioxgn/go-bot/commands/cotacao"
+	_ "github.com/fabioxgn/go-bot/commands/example"
+	_ "github.com/fabioxgn/go-bot/commands/gif"
+	_ "github.com/fabioxgn/go-bot/commands/megasena"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	config := newConfig()
+	log.Printf("%v\n", config)
+	bot.Run(config)
+}
+
+func newConfig() *bot.Config {
+	if os.Getenv("ENV") == "production" {
+		return productionConfig()
+	} else {
+		return developmentConfig()
+	}
+
+}
+
+func productionConfig() *bot.Config {
+	return &bot.Config{
+		Server:   os.Getenv("IRC_SERVER"),
+		Channels: strings.Split(os.Getenv("IRC_CHANNELS"), ","),
+		User:     os.Getenv("IRC_USER"),
+		Nick:     os.Getenv("IRC_NICK"),
+		Password: os.Getenv("IRC_PASSWORD"),
+		UseTLS:   true,
+	}
+}
+
+func developmentConfig() *bot.Config {
+	return &bot.Config{
+		Server:   "irc.freenode.org:7000",
+		Channels: []string{"#go-bot"},
+		User:     "go-bot-dev",
+		Nick:     "go-bot-dev",
+		UseTLS:   true,
+	}
+}
