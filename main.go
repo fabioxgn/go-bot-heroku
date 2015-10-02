b363c29f3dab56b85500fd0d55015cb433f0300a
Fábio Gomes <fabioxgn@gmail.com>
2015-10-02 08:37:22 -0300

    Adds crypto plugin

--- main.go
+++ main.go
@@ -9,18 +9,19 @@ import (
 	_ "github.com/go-chat-bot/plugins/catfacts"
 	_ "github.com/go-chat-bot/plugins/catgif"
 	_ "github.com/go-chat-bot/plugins/chucknorris"
+	_ "github.com/go-chat-bot/plugins/cnpj"
 	_ "github.com/go-chat-bot/plugins/cotacao"
+	_ "github.com/go-chat-bot/plugins/cpf"
+	_ "github.com/go-chat-bot/plugins/crypto"
 	_ "github.com/go-chat-bot/plugins/dilma"
+	_ "github.com/go-chat-bot/plugins/encoding"
 	_ "github.com/go-chat-bot/plugins/example"
 	_ "github.com/go-chat-bot/plugins/gif"
 	_ "github.com/go-chat-bot/plugins/godoc"
+	_ "github.com/go-chat-bot/plugins/guid"
 	_ "github.com/go-chat-bot/plugins/megasena"
 	_ "github.com/go-chat-bot/plugins/puppet"
 	_ "github.com/go-chat-bot/plugins/url"
-	_ "github.com/go-chat-bot/plugins/guid"
-	_ "github.com/go-chat-bot/plugins/cpf"
-	_ "github.com/go-chat-bot/plugins/cnpj"
-	_ "github.com/go-chat-bot/plugins/encoding"
 )
 
 func main() {
