79bdb5a2acc45d33d9d5fe3d3b08f76fb191df66
Fábio Gomes <fabioxgn@gmail.com>
2017-03-16 09:43:48 -0300

    Atualiza plugins

--- vendor/github.com/go-chat-bot/plugins-br/cotacao/cotacao.go
+++ vendor/github.com/go-chat-bot/plugins-br/cotacao/cotacao.go
@@ -8,18 +8,16 @@ import (
 )
 
 var (
-	url = "https://economia.awesomeapi.com.br/json/all"
+	url = "http://api.fixer.io/latest?base=BRL"
 )
 
 type retorno struct {
-	Dolar struct {
-		Cotacao  string `json:"ask"`
-		Variacao string `json:"varBid"`
-	} `json:"USD"`
-	Euro struct {
-		Cotacao  string `json:"ask"`
-		Variacao string `json:"varBid"`
-	} `json:"EUR"`
+	Real struct {
+		USD float32 `json:"USD"`
+		EUR float32 `json:"EUR"`
+		CAD float32 `json:"CAD"`
+		GBP float32 `json:"GBP"`
+	} `json:"rates"`
 }
 
 func cotacao(command *bot.Cmd) (msg string, err error) {
@@ -29,9 +27,11 @@ func cotacao(command *bot.Cmd) (msg string, err error) {
 		return "", err
 	}
 
-	return fmt.Sprintf("Dólar: %s (%s), Euro: %s (%s)",
-		data.Dolar.Cotacao, data.Dolar.Variacao,
-		data.Euro.Cotacao, data.Euro.Variacao), nil
+	return fmt.Sprintf("Dólar: %.2f, Euro: %.2f, CAD: %.2f, Libra: %.2f",
+		1 / data.Real.USD,
+		1 / data.Real.EUR,
+		1 / data.Real.CAD,
+		1 / data.Real.GBP), nil
 }
 
 func init() {
