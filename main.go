93a11059717c1ce4ede2b0b7baea3b6cfcee3a34
Fabio Gomes <fabioxgn@gmail.com>
2018-10-08 07:38:16 -0300

    Updates to go modules

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	"log"
+import (	
 	"os"
-	"strings"
-
-	"github.com/go-chat-bot/bot/irc"
+		
 	"github.com/go-chat-bot/bot/slack"
 	"github.com/go-chat-bot/bot/telegram"
 	_ "github.com/go-chat-bot/plugins-br/cnpj"
@@ -30,41 +27,6 @@ import (
 )
 
 func main() {
-	config := newConfig()
-	log.Printf("%v\n", config)
-	go irc.Run(config)
 	go telegram.Run(os.Getenv("TELEGRAM_TOKEN"), os.Getenv("DEBUG") != "")
 	slack.Run(os.Getenv("SLACK_TOKEN"))
-}
-
-func newConfig() *irc.Config {
-	if os.Getenv("ENV") == "production" {
-		return productionConfig()
-	} else {
-		return developmentConfig()
-	}
-
-}
-
-func productionConfig() *irc.Config {
-	return &irc.Config{
-		Server:   os.Getenv("IRC_SERVER"),
-		Channels: strings.Split(os.Getenv("IRC_CHANNELS"), ","),
-		User:     os.Getenv("IRC_USER"),
-		Nick:     os.Getenv("IRC_NICK"),
-		Password: os.Getenv("IRC_PASSWORD"),
-		UseTLS:   true,
-		Debug:    os.Getenv("DEBUG") != "",
-	}
-}
-
-func developmentConfig() *irc.Config {
-	return &irc.Config{
-		Server:   "irc.freenode.net:6697",
-		Channels: []string{"#go-bot"},
-		User:     "go-bot-dev",
-		Nick:     "go-bot-dev",
-		UseTLS:   true,
-		Debug:    true,
-	}
 }
